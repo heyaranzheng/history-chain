6afd91e909985497993f145757d62a37f424b87c
Aran Z <heyaranzheng@hotmail.com>
2025-10-02 12:20:19 +0800

    update

--- src/manager.rs
+++ src/manager.rs
@@ -1,5 +1,7 @@
+use tokio::sync::Mutex;
+use std::sync::Arc;
 
-use crate::block::{ Block, Digester};
+use crate::block::{ Block, Digester, BlockArgs };
 use crate::hash::HashValue;
 use crate::chain::{Chain, BlockChain, ChainInfo};
 use crate::herrors::HError;
@@ -11,10 +13,16 @@ use async_trait::async_trait;
 pub trait Manager {
     type DataBlock: Block ;
     type DigestBlock: Block + Digester;
-    async fn insert_block(&mut self, block: &Self::DataBlock) -> Result<(), HError>;
-    async fn verify_block(&mut self, block: &Self::DataBlock) -> Result<(), HError>;
     async fn archive(&mut self, keeper: &mut ChainKeeper<Self::DataBlock, Self::DigestBlock>) 
         -> Result<(), HError>;
+    async fn create_block<T>(&self, args: T) -> Result<Self::DataBlock, HError>
+        where T: BlockArgs;
+    ///in oreder to create a new block, we need to provide the previous block's hash
+    ///and index in the whole chain.
+    async fn hash_and_index(&self, chain: &BlockChain<Self::DataBlock>) -> 
+        Result<(HashValue, usize), HError>;
+    async fn add_chain(&mut self) -> Result<usize, HError>;
+
 }
 
 
@@ -24,6 +32,7 @@ pub struct ChainManager < B, D>
 
 {
     keeper: ChainKeeper<B, D>,
+    chain_buf: Arc<Mutex<BlockChain<B>>>,
 }
 
 impl < B, D> ChainManager<B, D> 
