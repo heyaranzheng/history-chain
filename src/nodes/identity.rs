f5b29748ab44cb8932c6b4cbf73a1a3eab7b6ff2
Aran Z <heyaranzheng@hotmail.com>
2025-11-09 23:09:41 +0800

    update

--- src/nodes/identity.rs
+++ src/nodes/identity.rs
@@ -18,10 +18,16 @@ pub struct SignRequest {
     pub response: oneshot::Sender<Result<[u8; 64], HError>>,
 }
 
+
+
+
+
+
 ///we can use this handle to send sign requests to the signer
 pub struct SignHandle {
     sender: Sender<SignRequest>,
 }
+
 
 ///An identity is a public key and a secret key
 pub struct Identity{
@@ -118,6 +124,8 @@ impl Clone for Identity {
 
 
 mod tests {
+    use std::thread::spawn;
+
     use super::*;
 
     #[test]
@@ -129,6 +137,48 @@ mod tests {
         Ok(())
     }
 
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn  test_singer() {
+        let id = Identity::new();
+        let public_key = id.public_key_to_bytes();
+
+        let handle = id.init_singer().await.unwrap();
+        let handle_clone = SignHandle { sender: handle.sender.clone() };
+        let msg = b"hello world";
+        let signature = sgin_v2_test(msg, handle).await.unwrap();
+  
+        let result =Identity::verify_signature_bytes(
+            msg, 
+            &public_key, 
+            &signature);
+        assert_eq!(result.is_ok(), true);
+
+       
+        spawn(async move ||  {
+                let msg2 = b"hello world, wow!";
+            let signature2 = sgin_v2_test(msg, handle_clone).await.unwrap();
     
+            let result =Identity::verify_signature_bytes(
+                msg2, 
+                &public_key, 
+                &signature2);
+            assert_eq!(result.is_ok(), true);
+            }
+
+        );
+    } 
+    pub async fn sgin_v2_test(bytes: &[u8], handle: SignHandle) -> Result<[u8; 64], HError> {
+        let (tx,  rx) = 
+            oneshot::channel::<Result<[u8; 64], HError>>();
+        let request = SignRequest {
+            bytes_vec: bytes.to_vec(),
+            response: tx,
+        };
+        let _ = handle.sender.send(request).await.unwrap();
+        let signature = rx.await.unwrap().unwrap();
+        Ok(signature)
+        
+    }
 
 }
